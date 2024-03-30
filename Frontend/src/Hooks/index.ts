55eca2fe4737da036cb14518d65a4c943455e7b5
Yash Patel <109963122+PatelYash7@users.noreply.github.com>
2024-03-30 18:38:56 +0530

    Added Single Blog Card

--- Frontend/src/Hooks/index.ts
+++ Frontend/src/Hooks/index.ts
@@ -32,3 +32,26 @@ export const useBlogs = () => {
         blogs
     }
 }
+
+export const useBlog = ({ id }: { id: string }) => {
+    const [loading, setLoading] = useState(true);
+    const [blog, setBlog] = useState<Blog>();
+
+    useEffect(() => {
+        axios.get(`${Backend_URL}/api/v1/blog/${id}`, {
+            headers: {
+                Authorization: localStorage.getItem("token")
+            }
+        })
+            .then(response => {
+                setBlog(response.data);
+                setLoading(false);
+            })
+    }, [id])
+
+    return {
+        loading,
+        blog
+    }
+
+}
--- Frontend/src/Pages/Blog.tsx
+++ Frontend/src/Pages/Blog.tsx
@@ -1,9 +1,30 @@
 
+import {useParams} from "react-router-dom";
+import { Loading } from "../Components/Loading";
+import { Appbar } from "../Components/Appbar";
+import { useBlog } from "../Hooks";
+import {ExpandBlog} from "../Components/ExpandBlog";
 
-function Blog() {
-  return (
-    <div>Blog</div>
-  )
+
+export  const Blog = () => {
+    const { id } = useParams();
+    const {loading, blog} = useBlog({
+        id: id || ""
+    });
+
+    if (loading || !blog) {
+        return <div>
+            <Appbar />
+        
+            <div className="flex flex-col justify-center h-screen">
+                
+                <div className="flex justify-center">
+                    <Loading />
+                </div>
+            </div>
+        </div>
+    }
+    return <div>
+        <ExpandBlog blog={blog} />
+    </div>
 }
-
-export default Blog
