8fd284e2e8cf7b130dbfcddd9c96ebb0e535943a
Yash Patel <109963122+PatelYash7@users.noreply.github.com>
2024-03-30 19:47:28 +0530

    Updated Appbar component

--- Frontend/src/Components/Appbar.tsx
+++ Frontend/src/Components/Appbar.tsx
@@ -2,16 +2,21 @@ import { Avatar } from "./BlogCard"
 import { Link } from "react-router-dom"
 
 export const Appbar = () => {
-    return <div className="flex justify-between px-10 py-4 border-b">
-        <Link to={'/blogs'} className="flex flex-col justify-center cursor-pointer">
-                Medium
+    return <div className="flex justify-between px-10 border-b py-">
+        <Link to={'/blogs'} className="flex flex-col justify-center text-xl font-bold cursor-pointer">
+                Blogosphere
         </Link>
-        <div>
+        <div className="flex items-center justify-center"> 
+
+            <div className="flex items-center pt-3">
+
             <Link to={`/publish`}>
+
                 <button type="button" className="mr-4 text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 ">New</button>
             </Link>
+            </div>
 
-            <Avatar size={"big"} name="harkirat" />
+            <Avatar size={"big"} name="Admin" />
         </div>
     </div>
 }
--- Frontend/src/Components/BlogCard.tsx
+++ Frontend/src/Components/BlogCard.tsx
@@ -45,10 +45,14 @@ export function Circle() {
     </div>
 }
 
-export function Avatar({ name, size = "small" }: { name: string, size?: "small" | "big" }) {
+export function Avatar({  size = "small" }: { name: string, size?: "small" | "big" }) {
     return <div className={`relative inline-flex items-center justify-center overflow-hidden bg-gray-600 rounded-full ${size === "small" ? "w-6 h-6" : "w-10 h-10"}`}>
-    <span className={`${size === "small" ? "text-xs" : "text-md"} font-extralight text-gray-600 dark:text-gray-300`}>
-        {name[0]}
+    <span className={` font-extralight text-white dark:text-gray-300`}>
+        
+<div className={`relative  ${size==="small" ? "w-8 h-8" : "w-10 h-10"} overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600`}>
+    <svg className={`${size==="small" ? "w-8 h-8 px-1" : "w-10 h-10"}  text-gray-400 `} fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path></svg>
+</div>
+
     </span>
 </div>
 }
